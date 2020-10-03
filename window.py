0d590308fb8011b7efb0543a5603429717677157
Michael Doise <mikedoise@icloud.com>
2020-10-03 00:12:02 -0500

    Added left and right aligned GUI boxes. May change in a different version as text labels are not aligned with edit fields.

--- window.py
+++ window.py
@@ -6,36 +6,60 @@ class UserInput(wx.Frame):
 
         # define the panel and the sizer for elements
         panel = wx.Panel(self)
-        box = wx.BoxSizer(wx.VERTICAL)
+        box = wx.BoxSizer(wx.HORIZONTAL)
 
+        # Add objects to left or right boxes. Labels on the left and text boxes on the right. This may change in the future ad labels and boxes are not always aligned.
+        leftBox = wx.BoxSizer(wx.VERTICAL)
+        rightBox = wx.BoxSizer(wx.VERTICAL)
 
 # starting to define the GUI elements
+        apiLabel = wx.StaticText(panel)
+        text = "API Key:"
+        apiLabel.SetLabel(text)
+        leftBox.Add(apiLabel)
         apiKey = wx.TextCtrl(panel)
-        text = "please type your API key:"
+        text = ""
         apiKey.SetLabelText(text)
-        box.Add(apiKey)
+        rightBox.Add(apiKey)
 
+        titleLabel = wx.StaticText(panel)
+        text = "Notification Title:"
+        titleLabel.SetLabel(text)
+        leftBox.Add(titleLabel)
         notificationTitle = wx.TextCtrl(panel)
         text = "notification title:"
         notificationTitle.SetLabel(text)
-        box.Add(notificationTitle)
+        rightBox.Add(notificationTitle)
 
+        topicLabel = wx.StaticText(panel)
+        text = "Notification Topic:"
+        topicLabel.SetLabel(text)
+        leftBox.Add(topicLabel)
         topic = wx.TextCtrl(panel)
-        text = "topic:"
+        text = ""
         topic.SetLabelText(text)
-        box.Add(topic)
+        rightBox.Add(topic)
 
+        bodyLabel = wx.StaticText(panel)
+        text = "Notification Body"
+        bodyLabel.SetLabel(text)
+        leftBox.Add(bodyLabel)
         body = wx.TextCtrl(panel,)
-        text = "body:"
+        text = ""
         body.SetLabel(text)
-        box.Add(body)
+        rightBox.Add(body)
 
+        soundLabel = wx.StaticText(panel)
+        text = "Sound:"
+        soundLabel.SetLabel(text)
+        leftBox.Add(soundLabel)
         sound = wx.TextCtrl(panel)
-        text = "sound:"
+        text = ""
         sound.SetLabel(text)
+        rightBox.Add(sound)
 
-
-
+        box.Add(leftBox)
+        box.Add(rightBox)
 
         panel.SetSizer(box)
 
