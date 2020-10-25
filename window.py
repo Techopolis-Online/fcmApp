2d7765742e1ad1486f9b0fef4d51414195c9577d
Michael Doise <mikedoise@icloud.com>
2020-10-25 15:12:02 -0500

    Added Import Button, Reset button, scrolling support and screen scaling.

--- window.py
+++ window.py
@@ -1,5 +1,6 @@
 # this is the main window class for wx python
 import wx
+import wx.lib.scrolledpanel
 from pyfcm import FCMNotification
 
 def push_to_topic(key, topic, title, body, sound):
@@ -8,8 +9,12 @@ def push_to_topic(key, topic, title, body, sound):
     return result
 
 class UserInput(wx.Frame):
-    def __init__(self, parent, title, style):
-        super(UserInput, self).__init__(parent, title=title, style=style)
+    def __init__(self, parent, title, style, size):
+        super(UserInput, self).__init__(parent, title=title, style=style, size=size)
+
+        screenSize = wx.DisplaySize()
+        screenWidth = screenSize[0]
+        screenHeight = screenSize[1]
 
         def onSubmitClicked(event):
             if apiKey.Value == "":
@@ -21,76 +26,111 @@ class UserInput(wx.Frame):
             dialog = wx.MessageDialog(None, "Push sent with the following result." + str(result), "Message result", wx.OK | wx.ICON_INFORMATION )
             dialog.ShowModal()
 
+        def onResetClicked(event):
+            topic.Value = ""
+            notificationTitle.Value = ""
+            body.Value = ""
+            sound.Value = ""
+
+        def OnImportClicked(event):
+            with wx.FileDialog(self, "Import API Key", "*.fbk files (*.bfk)|*.fbk", style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
+                if fileDialog.ShowModal() == wx.ID_CANCEL:
+                    return
+
+            pathname = fileDialog.GetPath()
+            try:
+                with open(pathname, 'r') as file:
+                    apiKey.Value = file.read()
+            except IOError:
+                wx.LogError("Cannot open file '%s'." % newfile)
+
         # define the panel and the sizer for elements
-        panel = wx.Panel(self)
+        panel = wx.lib.scrolledpanel.ScrolledPanel(self, -1, size=(screenWidth/4, screenHeight/2) )
+        panel.SetupScrolling()
         box = wx.BoxSizer(wx.VERTICAL)
 
-
         # starting to define the GUI elements
-        apilabel = wx.StaticText(panel)
+        hBox = wx.BoxSizer(wx.HORIZONTAL)
+        apilabel = wx.StaticText(panel, size=(-1, 20))
         text = "API Key"
         apilabel.SetLabelText(text)
-        box.Add(apilabel)
-        apiKey = wx.TextCtrl(panel)
+        hBox.Add(apilabel)
+        importButton = wx.Button(panel, -1, "Import Key", size=(-1, 20))
+        importButton.Bind(wx.EVT_BUTTON, OnImportClicked)
+        hBox.Add(importButton)
+        box.Add(hBox)
+        apiKey = wx.TextCtrl(panel, size=(screenWidth/4, 25))
         apiToolTip = wx.ToolTip("FCM API Key")
         apiKey.SetToolTip(apiToolTip)
         # text = "please type your API key:"
         # apiKey(text)
         box.Add(apiKey)
 
-        topicLabel = wx.StaticText(panel)
+
+        topicLabel = wx.StaticText(panel, size=(screenWidth/4, 20))
         topicLabel.SetLabelText("Notification Topic")
         box.Add(topicLabel)
-        topic = wx.TextCtrl(panel)
+        topic = wx.TextCtrl(panel, size=(screenWidth/4, 25))
         topicToolTip = wx.ToolTip("Notification Topic")
         topic.SetToolTip(topicToolTip)
         text = ""
         topic.SetLabelText(text)
         box.Add(topic)
 
-        titleLabel = wx.StaticText(panel)
+        titleLabel = wx.StaticText(panel, size=(screenWidth/4, 20))
         text = "Notification title:"
         titleLabel.SetLabelText(text)
         box.Add(titleLabel)
-        notificationTitle = wx.TextCtrl(panel)
+        notificationTitle = wx.TextCtrl(panel, size=(screenWidth/4, 25))
         text = ""
         titleToolTip = wx.ToolTip("Notification Title")
         notificationTitle.SetLabel(text)
         notificationTitle.SetToolTip(titleToolTip)
         box.Add(notificationTitle)
 
-        bodyLabel = wx.StaticText(panel)
+        bodyLabel = wx.StaticText(panel, size=(screenWidth/4, 20))
         bodyLabel.SetLabelText("Notification body")
         box.Add(bodyLabel)
-        body = wx.TextCtrl(panel,)
+        body = wx.TextCtrl(panel, size=(screenWidth/4, 60), style=wx.TE_MULTILINE)
         bodyToolTip = wx.ToolTip("Notification body")
         body.SetToolTip(bodyToolTip)
         text = ""
         body.SetLabel(text)
         box.Add(body)
 
-        soundLabel = wx.StaticText(panel)
+        soundLabel = wx.StaticText(panel, size=(screenWidth/4, 20))
         soundLabel.SetLabelText("Sound:")
         box.Add(soundLabel)
-        sound = wx.TextCtrl(panel)
+        sound = wx.TextCtrl(panel, size=(screenWidth/4, 25))
         soundToolTip = wx.ToolTip("Notification Sound")
         sound.SetToolTip(soundToolTip)
         text = ""
         sound.SetLabel(text)
         box.Add(sound)
 
+        hBox = wx.BoxSizer(wx.HORIZONTAL)
+        hBox.AddStretchSpacer(prop=1)
         submit = wx.Button(panel, -1, "Submit Push")
         submit.Bind(wx.EVT_BUTTON, onSubmitClicked)
-        box.Add(submit)
+        hBox.Add(submit, 0, wx.CENTER)
+
+        reset = wx.Button(panel, -1, "Reset")
+        reset.Bind(wx.EVT_BUTTON, onResetClicked)
+        hBox.Add(reset, 0, wx.CENTER)
+        hBox.AddStretchSpacer(prop=1)
+
+        box.Add(hBox, 0, wx.CENTER)
 
         panel.SetSizer(box)
 
-
-
         self.Show()
 
+        panel.Fit()
 
 if __name__ == '__main__':
     app = wx.App()
-    UserInput(None, title='fcmApp', style=wx.DEFAULT_DIALOG_STYLE)
+    screenSize = wx.DisplaySize()
+    screenWidth = screenSize[0]
+    screenHeight = screenSize[1]
+    UserInput(None, title='fcmApp', style=wx.DEFAULT_DIALOG_STYLE, size=(screenWidth/4, screenHeight/2))
     app.MainLoop()
